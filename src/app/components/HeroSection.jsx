a4f290bfbadabeb046ed4d094dc4da5cece5ff32
mkhizu21 <muhammad.khizar467@gmail.com>
2024-03-03 21:08:27 +0500

    changing

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -17,7 +17,7 @@ const HeroSection = () => {
         >
           <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-8xl lg:leading-normal font-extrabold">
             <span className="text-transparent bg-clip-text bg-gradient-to-r from-primary-400 to-secondary-600">
-              Hello, I'm{" "}
+              Hello, I&apos;m{" "}
             </span>
             <br></br>
             <TypeAnimation
@@ -38,7 +38,7 @@ const HeroSection = () => {
           </h1>
           <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl">
             I am a web developer and designer. creating modern and responsive
-            designs to Web. Let's work together and create something
+            designs to Web. Let&apos;s work together and create something
 
           </p>
           <div>
